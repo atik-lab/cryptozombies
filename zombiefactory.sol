4ad0bfd1a43206b693902d31b3b21f3638b5f1b4
Atik <atik.mail@protonmail.com>
2018-01-10 23:44:54 +0200

    Create zombiefactory.sol

---
+++ zombiefactory.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    // a normal program can listen to this blockchain event
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // mapping is like a dictionary, and address an eth address type
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        // call the event, so the listeners will get the notice of a new created zombie
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
